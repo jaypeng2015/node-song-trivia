f1588179928e51c7e91ff828f2d4f4fb2c3ab47d
Jingwei Peng <jay.peng@inlight.com.au>
2016-10-24 16:27:31 +1100

    Stop running web scrapper when the app starts.

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-const webScrapperConfig = require('./src/config').get('web-scrapper');
 const MrTrivia = require('./src/trivia');
 
 const mrTrivia = new MrTrivia();
-if (webScrapperConfig.activated) {
-  mrTrivia.study();
-}
-
 mrTrivia.listen();
